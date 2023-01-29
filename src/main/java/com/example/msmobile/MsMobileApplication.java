0ad39c16244ca9409742d39064d38346aa788662
cristiii <cristian.vlad10@gmail.com>
2023-01-29 15:40:05 +0200

    init

---
+++ src/main/java/com/example/msmobile/MsMobileApplication.java
@@ -0,0 +1,13 @@
+package com.example.msmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMobileApplication.class, args);
+    }
+
+}
